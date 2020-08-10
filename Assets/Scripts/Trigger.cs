4d663f3cc022e2d6f3d988fb00777c5068646b63
Baryshnikova Anastasiia <65864165+Holochka@users.noreply.github.com>
2020-08-10 14:53:07 +0300

    1
    1

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+
+    private void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+
+}
